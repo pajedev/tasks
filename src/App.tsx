f90daa40ece9e57ec1480ec1a1a9665790293357
Patricia Devine <pajedev@udel.edu>
2025-08-29 01:06:29 -0400

    Added name to App

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,12 @@ function App(): React.JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
+                UD CISC275 with React Hooks and TypeScript Author: Patricia
+                Devine
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
+                automatically reload. I hope this works!
             </p>
         </div>
     );
