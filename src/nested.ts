f72b1ac43bf46058dc6f6323708df322512414b2
Patricia Devine and Alaa Abu Hawzah <pajedev@udel.edu>
2025-09-22 20:56:54 -0400

    sum points passing test

--- src/nested.ts
+++ src/nested.ts
@@ -68,7 +68,12 @@ export function getNames(questions: Question[]): string[] {
  * Consumes an array of questions and returns the sum total of all their points added together.
  */
 export function sumPoints(questions: Question[]): number {
-    return 0;
+    let sum = questions.reduce(
+        (currentTotal: number, question: Question) =>
+            currentTotal + question.points,
+        0,
+    );
+    return sum;
 }
 
 /***
