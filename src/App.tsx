f64de9f6ed140b82a1207ee431f3111c32cdc4f3
Patricia Devine and Alaa Abu Hawzah <pajedev@udel.edu>
2025-09-25 19:14:12 -0400

    Counter and Reveal Answer functioning

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./App.css";
-import snoopy from "./images/comp_sci_snoopy.jpg";
-import { Container, Row, Col, Button } from "react-bootstrap";
-import { ChangeType } from "./components/ChangeType";
+// import snoopy from "./images/comp_sci_snoopy.jpg";
+// import { Container, Row, Col, Button } from "react-bootstrap";
+// import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
-import { StartAttempt } from "./components/StartAttempt";
-import { TwoDice } from "./components/TwoDice";
-import { CycleHoliday } from "./components/CycleHoliday";
+// import { StartAttempt } from "./components/StartAttempt";
+// import { TwoDice } from "./components/TwoDice";
+// import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
 
 function App(): React.JSX.Element {
@@ -21,14 +21,14 @@ function App(): React.JSX.Element {
             <Counter></Counter>
             <hr />
             <RevealAnswer></RevealAnswer>
-            <hr />
+            {/* <hr />
             <StartAttempt></StartAttempt>
             <hr />
             <TwoDice></TwoDice>
             <hr />
             <ChangeType></ChangeType>
             <hr />
-            <CycleHoliday></CycleHoliday>
+            <CycleHoliday></CycleHoliday> */}
         </div>
     );
 }
--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,17 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): React.JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [visible, setVisible] = useState<boolean>(true);
+
+    function flipVisibility(): void {
+        //opposite of previous value
+        setVisible(!visible);
+    }
+
+    return (
+        <div>
+            <Button onClick={flipVisibility}>Reveal Answer</Button>
+            {visible && <div>42</div>}
+        </div>
+    );
 }
