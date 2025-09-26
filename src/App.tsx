71f3ccfb5b9611b203694dfd4face2b5c0e02cf7
Patricia Devine and Alaa Abu Hawzah <pajedev@udel.edu>
2025-09-25 20:37:38 -0400

    start attempt functioning

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "./App.css";
 // import { Container, Row, Col, Button } from "react-bootstrap";
 import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
-// import { StartAttempt } from "./components/StartAttempt";
+import { StartAttempt } from "./components/StartAttempt";
 // import { TwoDice } from "./components/TwoDice";
 // import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
@@ -21,9 +21,9 @@ function App(): React.JSX.Element {
             <Counter></Counter>
             <hr />
             <RevealAnswer></RevealAnswer>
-            {/* <hr />
-            <StartAttempt></StartAttempt>
             <hr />
+            <StartAttempt></StartAttempt>
+            {/* <hr />
             <TwoDice></TwoDice> */}
             <hr />
             <ChangeType></ChangeType>
