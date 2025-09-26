cf5dcb89c8fca8e9b6f984b4268f0d189f399c6a
Patricia Devine and Alaa Abu Hawzah <pajedev@udel.edu>
2025-09-26 11:19:44 -0400

    Double Half behaving as expected

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,9 @@ import { TwoDice } from "./components/TwoDice";
 import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
 import { DoubleHalf } from "./bad-components/DoubleHalf";
-import { ColoredBox } from "./bad-components/ColoredBox";
-import { ShoveBox } from "./bad-components/ShoveBox";
-import { ChooseTeam } from "./bad-components/ChooseTeam";
+// import { ColoredBox } from "./bad-components/ColoredBox";
+// import { ShoveBox } from "./bad-components/ShoveBox";
+// import { ChooseTeam } from "./bad-components/ChooseTeam";
 
 function App(): React.JSX.Element {
     return (
@@ -22,14 +22,14 @@ function App(): React.JSX.Element {
                 Author: Patricia Devine
             </header>
             <hr></hr>
-            {/* <DoubleHalf></DoubleHalf> */}
+            <DoubleHalf></DoubleHalf>
             <hr></hr>
-            <ChooseTeam></ChooseTeam>
+            {/* <ChooseTeam></ChooseTeam>
             <hr></hr>
             <ColoredBox></ColoredBox>
             <hr></hr>
             <ShoveBox></ShoveBox>
-            <hr></hr>
+            <hr></hr> */}
             <Counter></Counter>
             <hr />
             <RevealAnswer></RevealAnswer>
--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -1,8 +1,13 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
-import { dhValue, setDhValue } from "./DoubleHalfState";
+//import { dhValue, setDhValue } from "./DoubleHalfState";
 
-function Doubler(): React.JSX.Element {
+interface changeValue {
+    setDhValue: (newValue: number) => void;
+    dhValue: number;
+}
+
+function Doubler({ setDhValue, dhValue }: changeValue): React.JSX.Element {
     return (
         <Button
             onClick={() => {
@@ -14,7 +19,7 @@ function Doubler(): React.JSX.Element {
     );
 }
 
-function Halver(): React.JSX.Element {
+function Halver({ setDhValue, dhValue }: changeValue): React.JSX.Element {
     return (
         <Button
             onClick={() => {
@@ -27,14 +32,15 @@ function Halver(): React.JSX.Element {
 }
 
 export function DoubleHalf(): React.JSX.Element {
+    const [dhValue, setDhValue] = useState<number>(10);
     return (
         <div>
             <h3>Double Half</h3>
             <div>
                 The current value is: <span>{dhValue}</span>
             </div>
-            <Doubler></Doubler>
-            <Halver></Halver>
+            <Doubler setDhValue={setDhValue} dhValue={dhValue}></Doubler>
+            <Halver setDhValue={setDhValue} dhValue={dhValue}></Halver>
         </div>
     );
 }
